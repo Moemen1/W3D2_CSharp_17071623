635163d13b6c23615ca3e99e2efa64bceafc2ddc
Moemen Badawi <moemenbadawi@hotmail.com>
2019-12-04 22:35:09 +0100

    Add project files.

---
+++ W3D2_CSharp_17071623/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace W3D2_CSharp_17071623
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext())
+            {
+                Console.Write("Wilt u records aanmaken?n/" +
+                              "Typ Ja of Nee: " );
+                string input = Console.ReadLine().ToLower();
+
+                Console.Write("Hoeveel records wilt u invullen?: ");
+                int count = Int32.Parse(Console.ReadLine());
+
+                for (int i = 0; i < count; i++)
+                {
+                    if (input == "ja")
+                    {
+                        Console.Write("Student voornaam: ");
+                        string voornaam = Console.ReadLine();
+
+                        Console.Write("Student achternaam: ");
+                        string achternaam = Console.ReadLine();
+
+                        Console.Write("Student leeftijd: ");
+                        int leeftijd = Int32.Parse(Console.ReadLine());
+
+                        var student = new Student
+                        {
+
+                            Voornaam = voornaam,
+                            Achternaam = achternaam,
+                            Leeftijd = leeftijd
+                        };
+
+                        /*context.Add(new Student()
+                            { Voornaam = "DummyStudent1", Achternaam = "DummmyAchternaam1", Leeftijd = 20 });
+                        context.Add(new Student()
+                            {Voornaam = "DummyStudent2", Achternaam = "AummmyAchternaam2", Leeftijd = 25 });
+                        context.Add(new Student()
+                            { Voornaam = "DummyStudent3", Achternaam = "ZummmyAchternaam3", Leeftijd = 23 });*/
+                        
+
+                        context.Add(student);
+                        context.SaveChanges();
+                        Console.WriteLine();
+                    }
+                }
+               
+                Console.WriteLine("\nAlle studenten in de database: ");
+
+                var StudentListOrderByNaam = context.Students.OrderBy(s => s.Achternaam).ToList(); //Ordered op achternaam ascending
+                var StudentListOuderDanTwintig = context.Students.Where(s => s.Leeftijd >= 20).ToList(); //Ouder dan 20 list
+
+                foreach (var student in StudentListOuderDanTwintig)
+                {
+                    Console.WriteLine(student);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ W3D2_CSharp_17071623/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace W3D2_CSharp_17071623
+{
+    class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public int Leeftijd { get; set; }
+
+        public override string ToString()
+        {
+            return $"id: {Id} | Voornaam: {Voornaam} | Achternaam: {Achternaam} | Leeftijd: {Leeftijd}";
+        }
+    }
+}
